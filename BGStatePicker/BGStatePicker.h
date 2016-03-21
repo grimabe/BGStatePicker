e3f4335420e89d9e0b43de2bd6edfd4421575233
Benjamin Grima <grima.benjamin@gmail.com>
2016-03-21 20:03:55 +0100

    init

---
+++ BGStatePicker/BGStatePicker.h
@@ -0,0 +1,19 @@
+//
+//  BGStatePicker.h
+//  BGStatePicker
+//
+//  Created by Benjamin Grima on 21/03/16.
+//  Copyright © 2016 Benjamin Grima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BGStatePicker.
+FOUNDATION_EXPORT double BGStatePickerVersionNumber;
+
+//! Project version string for BGStatePicker.
+FOUNDATION_EXPORT const unsigned char BGStatePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BGStatePicker/PublicHeader.h>
+
+
